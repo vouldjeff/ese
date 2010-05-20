34a7e51460ecc7bca2195a7f44c7fc7dc844e16f
Dimitar Vouldjeff <vouldjeff@gmail.com>
2010-05-20 00:29:30 -0700

    just one fix... not cool, but for a moment

--- app/models/result.rb
+++ app/models/result.rb
@@ -5,32 +5,4 @@ class Result < ActiveRecord::Base
   serialize :answers
 
   attr_accessible :answers, :result
-
-  def calculate
-    points = 0
-    max = 0
-    correctPerAnswer = 0
-
-    if answers == nil
-      return 0
-    end
-
-    if answers.length != right.length
-      return 0
-    end
-
-    right.each do |question|
-      max += question.weight
-      if answers["#{question.id}"] != nil and answers["#{question.id}"][answers["#{question.id}"].length - 1][:service] == "1"
-        total_correct_per_answer = question.num_of_correct
-        if total_correct_per_answer >= answers["#{question.id}"].length - 1 and answers["#{question.id}"].length - 1 > 0
-          correct = (question.correct.collect! { |e| e.id } & answers["#{question.id}"][0..(answers["#{question.id}"].length - 2)].collect { |e| e.to_i }).length
-          points += (correct / total_correct_per_answer) * question.weight
-        end
-      else
-        return 0
-      end
-    end
-    (points.to_f * 100 / max.to_f).round(2)
-  end
 end
